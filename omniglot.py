de1a5681feaee5614b43573bd327d7619be1ed8d
Victor I. Afolabi <javafolabi@gmail.com>
2018-07-10 21:37:05 +0100

    Utility files

---
+++ omniglot.py
@@ -0,0 +1,19 @@
+"""Omniglot dataset helper file.
+
+  @description
+    For visualizing, pre-processing and loading the Omniglot dataset.
+
+   @author
+     Victor I. Afolabi
+     Artificial Intelligence & Software Engineer.
+     Email: javafolabi@gmail.com
+     GitHub: https://github.com/victor-iyiola/
+
+   @project
+     File: utils.py
+     Created on 18 May, 2018 @ 5:26 PM.
+
+   @license
+     MIT License
+     Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
+"""
---
+++ utils.py
@@ -0,0 +1,23 @@
+"""Utility module for loading dataset..
+
+   @author
+     Victor I. Afolabi
+     Artificial Intelligence & Software Engineer.
+     Email: javafolabi@gmail.com
+     GitHub: https://github.com/victor-iyiola/
+
+   @project
+     File: utils.py
+     Created on 18 May, 2018 @ 5:26 PM.
+
+   @license
+     MIT License
+     Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
+"""
+import pickle
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+            
