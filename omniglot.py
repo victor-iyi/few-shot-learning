afe768a535df65c39d0ff58ed59a9f986ab2ddec
Victor I. Afolabi <javafolabi@gmail.com>
2018-07-11 00:35:10 +0100

    Complicated figures

--- omniglot.py
+++ omniglot.py
@@ -21,6 +21,7 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 from PIL import Image
 
@@ -82,25 +83,120 @@ def load_image(path: str, dtype: np.dtype=np.float32,
 
     return image
 
+# Helper function to plot images and labels.
 
-def visualize(image: np.ndarray, title: str=None, **kwargs):
+
+def visualize(train_dir: str, test_dir: str=None, **kwargs):
+    # Keyword arguments.
+    smooth = kwargs.get('smooth', True)
+    test_img = kwargs.get('test_img', None)
+    train_img = kwargs.get('train_img', None)
+
+    # Load images.
+    train = np.array([load_image(os.path.join(train_dir, f))
+                      for f in os.listdir(train_dir)])
+
+    if test_dir is not None:
+        test = np.array([load_image(os.path.join(test_dir, f))
+                         for f in os.listdir(test_dir)])
+
+    # Entire figure.
+    gs = gridspec.GridSpec(1, 2)
+
+    # Containing train_img and test_img.
+    gs_label = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[0])
+
+    # Containing matching images from train_img & test_img.
+    test_img_ax = plt.subplot(gs_label[0, 0])
+    train_img_ax = plt.subplot(gs_label[1, 0])
+
+    # Matching train & test images.
+    test_img_ax.imshow(test_img)
+    train_img_ax.imshow(train_img)
+
+    # Containing all images from train_dir and test_dir
+    gs_imgs = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[1])
+
+    # Containing all images from test_dir.
+    gs_imgs_test = gridspec.GridSpecFromSubplotSpec(5, 4,
+                                                    subplot_spec=gs_imgs[0])
+    # Containing all images from train_dir.
+    gs_imgs_train = gridspec.GridSpecFromSubplotSpec(5, 4,
+                                                     subplot_spec=gs_imgs[1])
+
+    # Image dimensions.
+    img_shape = train.shape
+    img_batch, img_size = img_shape[0], img_shape[1]
+    img_channel = img_shape[-3] if len(img_shape) > 3 else 1
+
+    # Create figure with sub-plots.
+    fig, axes = plt.subplots(5, 4)
+
+    # Adjust vertical spacing if we need to print ensemble and best-net.
+    wspace, hspace = 0.2, 0.4
+    fig.subplots_adjust(hspace=hspace, wspace=wspace)
+
+    for i, ax in enumerate(axes.flat):
+        # cmap type.
+        cmap = 'gray' if img_channel == 1 else None
+        # Interpolation type.
+        smooth = 'spline16' if smooth else 'nearest'
+
+        # Reshape image based on channel.
+        if img_channel == 1:
+            img = train[i].reshape(img_size, img_size)
+        else:
+            img = np.transpose(train[i], (1, 2, 0))
+
+        # Plot image.
+        ax.imshow(img, interpolation=smooth, cmap=cmap)
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
+def imshow(image: np.ndarray, title: str='', **kwargs):
     # Extract default keyword arguments.
-    cmap = kwargs.get("cmap") or "gray"
+    cmap = kwargs.get('cmap') or 'gray'
+    smooth = kwargs.get('smooth', False)
+    show = kwargs.get('show', True)
+
+    # Interpolation type.
+    smooth = 'spline16' if smooth else 'nearest'
 
     # Update keyword arguments.
-    kwargs.update({"cmap": cmap})
+    kwargs.update({
+        "cmap": cmap, "interpolation": smooth
+    })
 
     # Add image to plot.
     plt.imshow(image, **kwargs)
 
-    if title:
-        plt.title(title)
-    plt.show()
+    # Image title.
+    plt.title(title)
+
+    # Remove ticks from the plot.
+    plt.xticks([])
+    plt.yticks([])
+
+    # Maybe show image.
+    if show:
+        plt.show()
 
 
 if __name__ == '__main__':
+    test_dir = os.path.join(data_dir, 'run01/test')
+
     test_file = 'all_runs/run01/test/item01.png'
     train_file = 'all_runs/run01/training/class_labels.png'
-    image = load_image(test_file)
-    # print(image.shape)
-    visualize(image)
+    # image = load_image(test_file)
+    # imshow(image)
+    images = np.array([load_image(os.path.join(test_dir, f))
+                       for f in os.listdir(test_dir)])
+
+    visualize(test_dir, title="Run 0")
